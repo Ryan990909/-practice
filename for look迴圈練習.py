71e2e9a90cb647ff259e1c0102c152bf20628cae
Ryan990909 <86942073+Ryan990909@users.noreply.github.com>
2021-07-08 13:17:55 +0800

    Add files via upload

---
+++ for look迴圈練習.py
@@ -0,0 +1,17 @@
+for index in [1,2,3,4]:
+    print(index)
+    
+for index in range(10):    
+    print(index)
+
+for x in range(1, 15):
+    print('X:',x)
+
+for x in [2, 3, 4]:
+    print('X:',x)
+    
+for x in range(3,50,3):
+    print('x:',x)
+
+for i in range(1,10):
+    print('*'*i)
